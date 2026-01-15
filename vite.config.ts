b0ce8a87d6284cd68c947319add1d274a2ba121d
nguyendanguyenphuong2991-hash <nguyendanguyenphuong2991@gmail.com>
2026-01-15 16:15:42 +0700

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({ 
-  base: '/birthday-cake/',  
-  plugins: [
-    react({
+export default defineConfig({
+  base: '/birthday-cake/',
+  plugins: [react()],
+})
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
